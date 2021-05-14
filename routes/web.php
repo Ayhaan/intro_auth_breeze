bcf570a21e802d88262b0c7aef896e68056e83a2
Ayhan <caliskanayhan@hotmail.fr>
2021-05-14 11:30:39 +0200

    intro auth breeze

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+
+
+//back
+Route::get('/admin/dashboard', function () {
+    return view('admin/dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+//Auth route
+require __DIR__.'/auth.php';    
